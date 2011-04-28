d50f187876098a03fa7fca49a1de9004f8448725
ros <ros@Robotter>
2011-04-28 13:23:21 +0200

    Distanz über Grund scheint zu funktionieren. Jetzt unter Verwendung von CvBlob.

--- FigureFrame.h
+++ FigureFrame.h
@@ -8,7 +8,10 @@
 #ifndef FIGUREFRAME_H
 #define	FIGUREFRAME_H
 
+#include <opencv/cv.h>
+
 #include "MinMax.h"
+#include "XY.h"
 
 class FigureFrame {
 public:
@@ -17,9 +20,10 @@ public:
     FigureFrame(const FigureFrame& orig);
     virtual ~FigureFrame();
     
+public:
     CvPoint GetCenterPoint();
     int GetDistanceFromKinect();
-    void Draw(CvArr* img, CvScalar Color, int Thickness = 1);
+    void Draw(CvArr* img, int Thickness = 1);
     void DrawAsMask(CvArr* img);
     
     static IplImage* depthImage; 
@@ -28,9 +32,18 @@ public:
     
     bool IsInside(int x, int y);
     int Index() { return index; }
+    
+    CvPoint GetCenterOfGravity();
+
+    bool IsValid();
+
+public:
+    cv::Mat Matrix;
+    
 private:
     int x, y, width, height, index;
     CvFont font;
+    CvMoments moments;
 };
 
 #endif	/* FIGUREFRAME_H */
