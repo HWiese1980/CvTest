f346a7b175604f44f8f61bb114d42222b6ce7c26
ros <ros@Robotter>
2011-05-03 20:44:39 +0200

    Mit leisen Schritten richtung Fluchtpunkt.

---
+++ Corners.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   Corners.h
+ * Author: ros
+ *
+ * Created on 3. Mai 2011, 19:56
+ */
+
+#ifndef CORNERS_H
+#define	CORNERS_H
+
+typedef enum {
+    TopLeft,
+            BottomLeft,
+            TopRight,
+            BottomRight
+} CornerPosition;
+
+#endif	/* CORNERS_H */
+
